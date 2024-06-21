040ddafb6c65062a371d12e1fc8b86e56376713a
bincell <vasram2706@gmail.com>
2024-06-22 00:50:51 +0400

    fix sfml submodule

--- src/platform/main.cpp
+++ src/platform/main.cpp
@@ -2,5 +2,12 @@
 
 int main(void) {
   sf::Window window(sf::VideoMode(sf::Vector2u(800, 600)), "My window");
+
+  while (window.isOpen()) {
+    sf::Event event = window.pollEvent();
+    if (eventClosed)
+      window.close();
+  }
+
   return 0;
 }
